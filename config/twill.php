febf52b384a31570e76d60a326913949e33be570
Mark Cameron <mark.cameron@devfactory.ch>
2023-02-06 11:23:53 +0100

    Add Twill CMS and configuration

---
+++ config/twill.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'locale' => 'fr',
+    'fallback_locale' => 'en',
+
+    'publish_date_24h' => true, // enable 24h format in publisher dates
+    'publish_date_format' => 'd F Y H:i', // format used by publication date pickers
+    'publish_date_display_format' => 'DD MMMM YYYY HH:mm', // format used when displaying the publication date
+];
