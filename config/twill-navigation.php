87264065f3dfa691fc3c221bdcfcd59f90c2a3aa
Mark Cameron <mark.cameron@devfactory.ch>
2023-02-07 09:11:26 +0100

    Add Buckets for homepage

--- config/twill-navigation.php
+++ config/twill-navigation.php
@@ -1,39 +1,22 @@
 <?php
 
 return [
-    // 'homepage' => [
-    //     'title' => 'Home',
-    //     // 'route' => 'homepage',
-    //     'buckets' => [
-    //         'homepage' => [
-    //             'name' => 'Home Stats',
-    //             'buckets' => [
-    //                 'home_stats' => [
-    //                     'name' => 'Home Stats',
-    //                     'bucketables' => [
-    //                         [
-    //                             'module' => 'stats',
-    //                             'name' => 'Stats',
-    //                             'scopes' => ['published' => true],
-    //                         ],
-    //                     ],
-    //                     'max_items' => 5,
-    //                 ],
-    //             ],
-    //         ],
-    //     ],
-    // ],
+    'homepage' => [
+        'title' => 'Home',
+        'route' => 'homepage',
 
-    // 'featured' => [
-    //     'title' => 'Features',
-    //     'route' => 'twill.featured.homepage',
-    //     'primary_navigation' => [
-    //         'homepage' => [
-    //             'title' => 'Homepage',
-    //             'route' => 'twill.featured.homepage',
-    //         ],
-    //     ],
-    // ],
+    ],
+
+    'featured' => [
+        'title' => 'Features',
+        'route' => 'twill.featured.homepage',
+        'primary_navigation' => [
+            'homepage' => [
+                'title' => 'Homepage',
+                'route' => 'twill.featured.homepage',
+            ],
+        ],
+    ],
 
     'stats' => [
         'title' => 'Statistiques',
