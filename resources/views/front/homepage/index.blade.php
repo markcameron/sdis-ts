20fa2e308b0054c865e866523b800fe3305fc23d
Mark Cameron <mark.cameron@devfactory.ch>
2023-02-07 16:53:37 +0100

    Added Intervention sectors and recruitment banner

--- resources/views/front/homepage/index.blade.php
+++ resources/views/front/homepage/index.blade.php
@@ -4,4 +4,26 @@
     <x-stats></x-stats>
     <x-interventions></x-interventions>
     <x-emergency-numbers></x-emergency-numbers>
+
+    <section class="grid md:grid-cols-2">
+        <div class="relative" id="intervention-sectors">
+            <h2
+                class="text-primary font-barlowCondensed absolute inset-x-0 pt-4 text-center text-4xl uppercase sm:pt-8 sm:text-5xl">
+                Notre secteur d'intervention
+            </h2>
+            <div>
+              @include('svg/sectors_ts')
+            </div>
+        </div>
+        <div class="relative">
+            <h2
+                class="font-barlowCondensed absolute inset-x-0 pt-4 text-center text-4xl uppercase text-white sm:pt-8 sm:text-5xl">
+                Devenez Sapeur Pompier !</h2>
+            <img src="{{ asset('images/recrutement-thumbnail.jpg') }}" class="w-full" alt="">
+            <div class="absolute inset-x-0 bottom-0 mb-16 text-center">
+                <a href=""
+                    class="bg-primary hover:bg-primary-dark rounded-md px-4 py-3 text-xl font-bold text-white">118-info.ch</a>
+            </div>
+        </div>
+    </section>
 @endsection
