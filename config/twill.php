124d238ec1f520e77c00bef3d372b0d447a25b2e
Mark Cameron <mark.cameron@devfactory.ch>
2023-02-07 13:14:44 +0100

    Don't put SVG images through Glide processing

--- config/twill.php
+++ config/twill.php
@@ -11,4 +11,27 @@ return [
     'publish_date_24h' => true, // enable 24h format in publisher dates
     'publish_date_format' => 'd F Y H:i', // format used by publication date pickers
     'publish_date_display_format' => 'DD MMMM YYYY HH:mm', // format used when displaying the publication date
+
+    'glide' => [
+        'original_media_for_extensions' => ['svg'],
+    ],
+
+    'buckets' => [
+        'homepage' => [
+            'name' => 'Home Stats',
+            'buckets' => [
+                'home_stats' => [
+                    'name' => 'Home Stats',
+                    'bucketables' => [
+                        [
+                            'module' => 'stats',
+                            'name' => 'Stats',
+                            'scopes' => ['published' => true],
+                        ],
+                    ],
+                    'max_items' => 5,
+                ],
+            ],
+        ],
+    ],
 ];
