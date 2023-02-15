af82a6f03bbf8eace42f9ef2152122ad8d8de971
Mark Cameron <mark.cameron@devfactory.ch>
2023-02-15 13:51:08 +0100

    Add system to handling processing Interventions received by email

---
+++ app/Enums/Village.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Enums;
+
+enum Village: string {
+    case BogisBossey = 'Bogis-Bossey';
+    case ChavannesDeBogis = 'Chavannes-de-Bogis';
+    case ChavannesDesBois = 'Chavannes-des-Bois';
+    case Commugny = 'Commugny';
+    case Coppet = 'Coppet';
+    case Founex = 'Founex';
+    case Mies = 'Mies';
+    case Tannay = 'Tannay';
+
+    public static function fromIntervention(string $village): ?self
+    {
+        return match($village)
+        {
+            'BOGIS-BOSSEY' => Village::BogisBossey,
+            'CHAVANNES-DE-BOGIS' => Village::ChavannesDeBogis,
+            'CHAVANNES-DES-BOIS' => Village::ChavannesDesBois,
+            'COMMUGNY' => Village::Commugny,
+            'COPPET' => Village::Coppet,
+            'FOUNEX' => Village::Founex,
+            'MIES' => Village::Mies,
+            'TANNAY' => Village::Tannay,
+            default => null,
+        };
+    }
+}
--- app/Services/InterventionService.php
+++ app/Services/InterventionService.php
@@ -2,14 +2,45 @@
 
 namespace App\Services;
 
+use App\Enums\Village;
 use App\Models\Intervention;
 use BeyondCode\Mailbox\InboundEmail;
 
 class InterventionService {
     public function createFromEmail(InboundEmail $email): void
     {
-        Intervention::create([
+        $intervention = Intervention::create([
             'title' => $email->text(),
+            'description' => $this->extractType($email->text()),
+            'type' => $this->extractType($email->text()),
+            'village' => $this->extractVillage($email->text()),
+            'date' => $email->date(),
         ]);
+
+        $this->publishIntervention($intervention);
+    }
+
+    private function publishIntervention(Intervention $intervention): void
+    {
+        $fields = collect([
+            $intervention->village,
+            $intervention->type,
+        ]);
+
+        $publishedStatus = $fields->filter()->count() === 2;
+
+        $intervention->update(['published' => $publishedStatus]);
+    }
+
+    public function extractType(string $text): string
+    {
+        preg_match('/.* - (.+) \(.*/mU', $text, $matches);
+        return $matches[1];
+    }
+
+    public function extractVillage(string $text): ?string
+    {
+        preg_match('/.*\((.+) -.*\).*/mU', $text, $matches);
+        return Village::fromIntervention($matches[1])?->value;
     }
 }
