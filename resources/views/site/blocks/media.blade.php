97f21fded6c2bd9fa2329f36632a568ec23d1ae1
Mark Cameron <mark.cameron@devfactory.ch>
2023-03-10 13:05:25 +0100

    Add link to original image for media blocks

--- resources/views/site/blocks/media.blade.php
+++ resources/views/site/blocks/media.blade.php
@@ -1,7 +1,8 @@
 @php
+    $imageOriginal = $block->imageAsArray('image', 'default');
     $image = $block->imageAsArray('image', 'default', ['w' => 992]);
 @endphp
 
-<a href="{{ $image['src'] }}">
+<a href="{{ $imageOriginal['src'] }}">
     <img src="{{ $image['src'] }}" class="w-full h-auto py-12" alt="{{ $image['alt'] }}" title="{{ $image['caption'] }}">
 </a>
